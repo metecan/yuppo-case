1dd4614be5b1726da3129d5a9550dceb09dd8c38
metecan <metecankaplan@hotmail.com>
2025-08-26 14:33:39 +0300

    [FEAT]: Implement EditorLayout component with styles and types

---
+++ src/components/layouts/EditorLayout/EditorLayout.tsx
@@ -0,0 +1,9 @@
+import { IEditorLayoutProps } from "./EditorLayout.types";
+import styles from "./EditorLayout.module.scss";
+
+const EditorLayout = (props: IEditorLayoutProps) => {
+  const { children } = props;
+  return <div className={styles.editorLayout}>{children}</div>;
+};
+
+export default EditorLayout;
---
+++ src/components/layouts/EditorLayout/EditorLayout.types.ts
@@ -0,0 +1,3 @@
+export interface IEditorLayoutProps {
+  children: React.ReactNode;
+}
