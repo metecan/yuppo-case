bfc672cc7a195207f6b5274bcdac2eca6c15872f
metecan <metecankaplan@hotmail.com>
2025-08-26 20:07:03 +0300

    [STYLE]: Refactor EditorLayout component for consistent styling and formatting

--- src/components/layouts/EditorLayout/EditorLayout.tsx
+++ src/components/layouts/EditorLayout/EditorLayout.tsx
@@ -1,8 +1,9 @@
-import { IEditorLayoutProps } from "./EditorLayout.types";
-import styles from "./EditorLayout.module.scss";
+import { IEditorLayoutProps } from './EditorLayout.types';
+import styles from './EditorLayout.module.scss';
 
 const EditorLayout = (props: IEditorLayoutProps) => {
   const { children } = props;
+
   return <div className={styles.editorLayout}>{children}</div>;
 };
 
--- src/components/layouts/EditorLayout/index.ts
+++ src/components/layouts/EditorLayout/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./EditorLayout";
+export { default } from './EditorLayout';
